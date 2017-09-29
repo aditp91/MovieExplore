44f2eda6edfba88791ffb1d1c9f16930cdab7c40
aditp91 <apothuri@synthesio.com>
2017-09-29 10:25:14 -0400

    feat(router): use express router for api calls

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const router = express.Router();
 
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
@@ -11,8 +12,28 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
+
+
+
+// route middleware that will happen on every request
+router.use((req, res, next) => {
+      // log each request to the console
+      console.log(req.method, req.url);
+      // continue doing what we were doing and go to the route
+      next();
+  });
+  
+router.get('/helloworld', (req, res) => {
+  res.json("successful response!");
+});
+
+app.use('/api', router);
+
+
+
+
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -4,11 +4,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 9000;
 
-app.get('/api/helloworld', (req, res) => {
-  console.log("trying to send back a res");
-  res.json("successful response!");
-});
-
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
